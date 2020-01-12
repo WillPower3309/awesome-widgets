7e11fdea24763ef0378b1239320b7abaed70c762
William McKinnon <willpower3309@gmail.com>
2020-01-12 12:41:07 -0500

    added wallpaper blur widget

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # awesome-widgets
 A collection of various widgets I've developed for awesomewm
+
+|Widget|Description|Dependencies|Notes|
+|:-:|:-:|:-:|:-:|
+|`wallpaper.lua`|Blurs wallpaper when windows are open|`imagemagick`|Change the `wallpaper` variable to match the name of your wallpaper|
---
+++ wallpaper.lua
@@ -0,0 +1,103 @@
+--      ██╗    ██╗ █████╗ ██╗     ██╗     ██████╗  █████╗ ██████╗ ███████╗██████╗ 
+--      ██║    ██║██╔══██╗██║     ██║     ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
+--      ██║ █╗ ██║███████║██║     ██║     ██████╔╝███████║██████╔╝█████╗  ██████╔╝
+--      ██║███╗██║██╔══██║██║     ██║     ██╔═══╝ ██╔══██║██╔═══╝ ██╔══╝  ██╔══██╗
+--      ╚███╔███╔╝██║  ██║███████╗███████╗██║     ██║  ██║██║     ███████╗██║  ██║
+--       ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝
+
+-- ===================================================================
+-- Imports
+-- ===================================================================
+
+
+local awful = require("awful")
+local gears = require("gears")
+local naughty = require("naughty")
+local beautiful = require('beautiful')
+local config_dir = gears.filesystem.get_configuration_dir()
+
+
+-- ===================================================================
+-- Initialization
+-- ===================================================================
+
+
+local blurred = false;
+
+local wallpaper = config_dir .. "/wallpaper.jpg"
+local blurredWallpaper = config_dir .. "/.cache/blurredWallpaper.jpg"
+
+-- Set Wallpaper
+gears.wallpaper.maximized(wallpaper)
+
+--- Check if a file or directory exists in this path
+local function exists(file)
+   local ok, err, code = os.rename(file, file)
+   if not ok then
+      if code == 13 then
+         -- Permission denied, but it exists
+         return true
+      end
+   end
+   return ok, err
+end
+
+-- check if blurred wallpaper needs to be created
+if not exists(blurredWallpaper) then
+  naughty.notify({
+     preset = naughty.config.presets.normal,
+     title = 'Wallpaper',
+     text = 'Generating blurred wallpaper...'
+   })
+   -- uses image magick to create a blurred version of the wallpaper
+   awful.spawn.with_shell("convert -filter Gaussian -blur 0x30 " .. wallpaper .. " " .. blurredWallpaper)
+end
+
+
+-- ===================================================================
+-- Functionality
+-- ===================================================================
+
+
+-- changes to blurred wallpaper
+local function blur()
+   if not blurred then
+      gears.wallpaper.maximized(blurredWallpaper)
+      blurred = true
+   end
+end
+
+-- changes to normal wallpaper
+local function unblur()
+   if blurred then
+      gears.wallpaper.maximized(wallpaper)
+      blurred = false
+   end
+end
+
+-- blur / unblur on tag change
+tag.connect_signal('property::selected', function(t)
+   -- if tag has clients
+   for _ in pairs(t:clients()) do
+      blur()
+      return
+   end
+   -- if tag has no clients
+   unblur()
+end)
+
+-- check if wallpaper should be blurred on client open
+client.connect_signal("manage", function(c)
+   blur()
+end)
+
+-- check if wallpaper should be unblurred on client close
+client.connect_signal("unmanage", function(c)
+   local t = awful.screen.focused().selected_tag
+   -- check if any open clients
+   for _ in pairs(t:clients()) do
+      return
+   end
+   -- unblur if no open clients
+   unblur()
+end)
